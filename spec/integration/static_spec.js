24279473ef7ae9b066fc119968c13a34899e8c91
Kelly Garry <kellygarry@KG-Macbook-Air.local>
2018-08-29 21:53:30 -0600

    ckpt4-assignment completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -6,26 +6,25 @@ describe("routes : static", () => {
 
   describe("GET /", () => {
 
-      it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
+      it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
 
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
-
         done();
       });
     });
   });
 
-  // describe("GET /marco", () => {
-  //
-  //   it("should return word 'polo'", (done) => {
-  //
-  //     request.get(base + "marco", (err, res, body) => {
-  //       expect(body).toContain('polo');
-  //
-  //       done();
-  //     });
-  //   });
-  // });
+  describe("GET /about", () => {
+
+    it("should return 'About Us' in the body of the response", (done) => {
+
+      request.get(base + "about", (err, res, body) => {
+        expect(body).toContain("About Us");
+        done();
+      });
+    });
+  });
+
 });
--- src/routes/static.js
+++ src/routes/static.js
@@ -3,10 +3,6 @@ const router = express.Router();
 const staticController = require("../controllers/staticController");
 
 router.get("/", staticController.index);
-
-// router.get("/marco", (req, res, next) => {
-//   res.send("polo");
-//   next()
-// });
+router.get("/about", staticController.about);
 
 module.exports = router;
