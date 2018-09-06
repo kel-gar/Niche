685ce2f81cadbbfa8172f55f58734a4d5ce29fb0
Kelly Garry <kellygarry@KG-Macbook-Air.local>
2018-09-05 21:41:56 -0600

    ckpt7-assignment completed

--- spec/unit/post_spec.js
+++ spec/unit/post_spec.js
@@ -5,23 +5,19 @@ const Post = require("../../src/db/models").Post;
 describe("Post", () => {
 
   beforeEach((done) => {
-//#1
     this.topic;
     this.post;
     sequelize.sync({force: true}).then((res) => {
-
-//#2
       Topic.create({
         title: "Expeditions to Alpha Centauri",
         description: "A compilation of reports from recent visits to the star system."
       })
       .then((topic) => {
         this.topic = topic;
-//#3
+
         Post.create({
           title: "My first visit to Proxima Centauri b",
           body: "I saw some rocks.",
-//#4
           topicId: this.topic.id
         })
         .then((post) => {
@@ -123,5 +119,5 @@ describe("Post", () => {
       });
 
     });
-    
+
 });
