004c7843c412b348966a1952c10f2559425955fb
Kelly Garry <kellygarry@KG-Macbook-Air.local>
2018-09-19 19:01:12 -0600

    final

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -6,11 +6,11 @@ describe("routes : static", () => {
 
   describe("GET /", () => {
 
-      it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
+      it("should return status code 200 and have 'Welcome to Community Chat' in the body of the response", (done) => {
 
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-        expect(body).toContain("Welcome to Bloccit");
+        expect(body).toContain("Welcome to Community Chat");
         done();
       });
     });
