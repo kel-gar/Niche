3b023aebed87d856bef7caffd5c7882e390d66f6
Kelly Garry <kellygarry@KG-Macbook-Air.local>
2018-10-10 10:25:57 -0600

    final

--- src/db/models/favorite.js
+++ src/db/models/favorite.js
@@ -16,7 +16,7 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "postId",
       onDelete: "CASCADE"
     });
-    
+
     Favorite.belongsTo(models.User, {
       foreignKey: "userId",
       onDelete: "CASCADE"
